363b0578a493999ef99e5235ebe8e4398467d421
Ramandeep singh <ramandeepsingh1511@gmail.com>
2025-08-26 11:05:54 +0530

    add some design change

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
+const allowedDevOrigins = [
+  "http://localhost:3000",
+  "http://127.0.0.1:3000",
+  "http://108.174.57.156"
+  // Add other allowed development origins here
+];
+
 const nextConfig: NextConfig = {
   /* config options here */
+  // Example: Expose allowedDevOrigins as a runtime config
+  publicRuntimeConfig: {
+    allowedDevOrigins,
+  },
 };
 
 export default nextConfig;
